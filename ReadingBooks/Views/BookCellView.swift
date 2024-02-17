046ff74ba993a313c4f63014b5735f0ac04ded85
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-17 16:25:24 +0200

    book detail view update in swift data p1

--- ReadingBooks/Views/BookCellView.swift
+++ ReadingBooks/Views/BookCellView.swift
@@ -11,17 +11,22 @@ struct BookCellView: View {
     let book: Book
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(book.title)
-                .bold()
-            HStack {
-                Text("Author: \(book.author)")
-                Spacer()
-                Text("Published on: \(book.publishedYear.description)")
+        NavigationLink(value: book) {
+            VStack(alignment: .leading) {
+                Text(book.title)
+                    .bold()
+                HStack {
+                    Text("Author: \(book.author)")
+                    Spacer()
+                    Text("Published on: \(book.publishedYear.description)")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                .padding(.top, 20)
             }
-            .font(.subheadline)
-            .foregroundStyle(.secondary)
-            .padding(.top, 20)
+        }
+        .navigationDestination(for: Book.self) { book in
+            BookDetailView(book: book)
         }
     }
 }
---
+++ ReadingBooks/Views/BookDetailView.swift
@@ -0,0 +1,54 @@
+//
+//  BookDetailView.swift
+//  ReadingBooks
+//
+//  Created by test on 17.02.2024.
+//
+
+import SwiftUI
+
+struct BookDetailView: View {
+    let book: Book
+    
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var isEditing = false
+    
+    @State private var title: String = ""
+    @State private var author: String = ""
+    @State private var publishedYear: Int? = nil
+    
+    init(book: Book) {
+        self.book = book
+        self._title = State.init(initialValue: book.title)
+        self._author = State.init(initialValue: book.author)
+        self._publishedYear = State.init(initialValue: book.publishedYear)
+    }
+    
+    var body: some View {
+        Form {
+            if isEditing {
+                Group {
+                    TextField("Book title", text: $title)
+                    TextField("Book author", text: $author)
+                    TextField("Published Year", value: $publishedYear, formatter: NumberFormatter())
+                        .keyboardType(.numberPad)
+                }
+                .textFieldStyle(.roundedBorder)
+            } else {
+                Text(book.title)
+                Text(book.author)
+                Text(book.publishedYear.description)
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(isEditing ? "Done" : "Edit") {
+                    isEditing.toggle()
+                }
+            }
+        }
+        .navigationTitle("Book Detail")
+    }
+}
