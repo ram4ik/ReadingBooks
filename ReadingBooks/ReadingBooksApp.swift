d197f0c177322f220f42b268df9bbd9f0a38b05e
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-17 11:39:10 +0200

    Initial Commit

---
+++ ReadingBooks/ReadingBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingBooksApp.swift
+//  ReadingBooks
+//
+//  Created by test on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReadingBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
